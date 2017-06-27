091a1fc9d71b2dabed2acbc82fa820505c0dcf79
Clint Brown <clint.brown.atx@gmail.com>
2017-06-27 18:31:34 -0500

    setup node module, creating floret.js
    restructuring

---
+++ index.js
@@ -0,0 +1,6 @@
+exports = module.exports = require('lib/floret.js');
+exports.auth = require('lib/floret-auth');
+exports.gateway = require('lib/floret-gateway');
+exports.pub = require('lib/floret-pub');
+exports.sub = require('lib/floret-sub');
+exports.channel = require('lib/floret-channel');
