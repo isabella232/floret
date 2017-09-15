f5ca5dc9fd4cf9780bec811527a0393817e5333b
Clint Brown <clint.brown.atx@gmail.com>
2017-09-15 10:18:05 -0500

    Consolidating repos

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
 exports = module.exports = require('./lib/floret.js');
-exports.gateway = require('floret-gateway');
-exports.pub = require('floret-publisher');
-exports.sub = require('floret-subscriber');
-exports.channel = require('floret-gatekeeper');
